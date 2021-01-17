224a99d24bc1792417d1fb54c5ff0458ae80be31
KimTaebin-ai <76577776+KimTaebin-ai@users.noreply.github.com>
2021-01-17 23:01:50 +0900

    page-info[set]

--- inPHRtaebin/src/app/service/page-info.service.ts
+++ inPHRtaebin/src/app/service/page-info.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PageInfoService {
 
-  constructor() { }
+  constructor(private router: Router) { 
+  }
+
+
+  getPageInfo(type: string) {
+    const pageInfo = '';
+    let value = '';
+    
+    if (pageInfo.length && type === 'title') {
+      // value = pageInfo[pageInfo.length - 1].title;
+    }
+  }
 }
