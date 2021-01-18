e960bfa1e5a7c4eddd6c212b77c7c245766e844c
KimTaebin-ai <76577776+KimTaebin-ai@users.noreply.github.com>
2021-01-19 00:39:20 +0900

    tab [set]

--- inPHRtaebin/src/app/app-routing.module.ts
+++ inPHRtaebin/src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  // },
   {
     path: '',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'diary',
     loadChildren: () => import('./pages/diary/diary.module').then( m => m.DiaryPageModule)
   },
-
- 
 ];
 
 @NgModule({
